5b20a95e14fba5db39ae338bf85692eb5e4727cd
PiVortex <owenhassall@hotmail.co.uk>
2023-11-21 14:22:22 +0000

    Added README.md

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from lib import Simulation
 
-sim = Simulation(n_excited = 1, n_sites = 5) # Create a new simulation 
+sim = Simulation(n_excited = 1, n_sites = 12) # Create a new simulation 
 print(sim.get_current_state())
 sim.apply_cycle()
 state = sim.get_current_state()
